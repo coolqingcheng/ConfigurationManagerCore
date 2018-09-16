dabe0d95f96955fa8fee2c4e22765f2b9cd709aa
家里 <qingchengcode@qq.com>
2018-09-16 21:58:36 +0800

    init

---
+++ ConfigurationManagerCore/ConfigurationManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigurationManagerCore
+{
+    public class ConfigurationManager
+    {
+        private static IConfigurationRoot config;
+        static ConfigurationManager()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("app.json");
+            config = builder.Build();
+        }
+
+        public static IConfigurationRoot AppSettings
+        {
+            get
+            {
+                return config;
+            }
+        }
+
+        public static string Get(string key)
+        {
+            return config[key];
+        }
+
+    }
+}
